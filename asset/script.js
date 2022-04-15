df13d52df108998f20b4be974f690df28efb565f
Tim Siu <anson242004@hotmail.com>
2022-04-15 19:13:19 +0930

    add content in html and css

---
+++ asset/script.js
@@ -0,0 +1,9 @@
+// search for a city
+
+//presented with current and future conditions for that city
+//city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+// UV index shows presented with a color that indicates whether the conditions are favorable, moderate, or severe
+// future weather conditions for that city
+// presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+
+// city is added to the search history
